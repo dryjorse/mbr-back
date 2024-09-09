c913632402deead855179039e571f186bb27402d
dryjorse <kozoevabdulazim@gmail.com>
2024-09-09 22:22:35 +0600

    ordering fix

--- payments/admin.py
+++ payments/admin.py
@@ -6,7 +6,7 @@ from django.utils.timezone import localtime
 @admin.register(Payment)
 class PaymentsAdmin(admin.ModelAdmin):
   list_display = ('summ', 'user', 'geolocation', 'formatted_created_at')
-  ordering = ['created_at']
+  ordering = ['-created_at']
   readonly_fields = ['receipt_number']
 
   def formatted_created_at(self, obj):
