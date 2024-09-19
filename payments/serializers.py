5d8313fb0102e6e4b0543057e27d29cc51acc913
dryjorse <116591223+dryjorse@users.noreply.github.com>
2024-09-19 16:33:13 +0600

    Update serializers.py fix 2

--- payments/serializers.py
+++ payments/serializers.py
@@ -4,8 +4,8 @@ from .models import Payment
 class PaymentSerializer(serializers.ModelSerializer):
   class Meta:
     model = Payment
-    fields = ['id', 'type', 'summ', 'fullname', 'phone', 'transport_code', 'geolocation', 'receipt_number', 'is_success', 'created_at' , 'users']
-    read_only_fields = ['user', 'created_at', 'receipt_number']
+    fields = ['id', 'type', 'summ', 'fullname', 'phone', 'transport_code', 'geolocation', 'receipt_number', 'is_success', 'created_at', 'users']
+    read_only_fields = ['created_at', 'receipt_number']
 
   def validate_summ(self, value):
     if value <= 0:
@@ -13,8 +13,11 @@ class PaymentSerializer(serializers.ModelSerializer):
     return value
 
   def create(self, validated_data):
+    # Получаем пользователя из контекста
     user = self.context['request'].user
-    users = validated_data.pop('users', None)
+    users = validated_data.pop('users', None)  # Получаем других пользователей из данных
+
+    # Проверяем баланс пользователя, если тип не "tulpar"
     summ = validated_data['summ']
     type = validated_data['type']
 
@@ -22,12 +25,18 @@ class PaymentSerializer(serializers.ModelSerializer):
       if user.balance < summ:
         raise serializers.ValidationError("Insufficient balance.")
       
+      # Вычитаем сумму из баланса пользователя
       user.balance -= summ
       user.save()
 
+    # Создаем объект Payment без поля users
     payment = Payment.objects.create(**validated_data)
 
+    # Добавляем текущего пользователя в поле users
+    payment.users.add(user)
+
+    # Если в запросе переданы другие пользователи, добавляем их
     if users:
-      payment.users.set(users)
-    
+      payment.users.add(*users)
+
     return payment
